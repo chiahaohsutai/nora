d42ba2f751826bbcfe9134768b46ba8eda0d469f
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-02-15 09:50:36 -0500

    added var decl tracking

--- src/compiler/parser/decls/var.rs
+++ src/compiler/parser/decls/var.rs
@@ -1,18 +1,21 @@
+use std::fmt::format;
+
 use tracing::{debug, instrument};
 
-use super::super::super::tokenizer::Token;
+use super::super::super::{generate_tag, tokenizer::Token};
 use super::super::{ParseResult, ParserState, exprs};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Decl {
+    id: String,
     name: String,
     expr: Option<exprs::Expr>,
 }
 
 impl Decl {
     #[rustfmt::skip]
-    fn new(name: String, expr: Option<exprs::Expr>) -> Self {
-        Self { name, expr }
+    fn new(id: String, name: String, expr: Option<exprs::Expr>) -> Self {
+        Self { id, name, expr }
     }
 }
 
@@ -21,19 +24,34 @@ pub fn parse(mut state: ParserState) -> ParseResult<Decl> {
     debug!("Consuming variable declaration");
     match state.tokens.pop_front() {
         Some(Token::Int) => match state.tokens.pop_front() {
-            Some(Token::Ident(ident)) => match state.tokens.pop_front() {
-                Some(Token::Semicolon) => Ok((state, Decl::new(ident, None))),
-                Some(Token::Eq) => {
-                    let (mut state, expr) = exprs::parse(state)?;
-                    match state.tokens.pop_front() {
-                        Some(Token::Semicolon) => Ok((state, Decl::new(ident, Some(expr)))),
-                        Some(token) => Err(format!("Expected `;` found: {token}")),
-                        None => Err("Unexpected end of input: expected `;`".into()),
+            Some(Token::Ident(ident)) => {
+                let id = generate_tag(format!("var.{ident}"));
+                match state.tokens.pop_front() {
+                    Some(Token::Semicolon) => {
+                        if let Some(_) = state.vars.insert(ident.clone(), id.clone()) {
+                            let e = format!("Duplicate variable declaration: {ident}");
+                            state.errors.push(e);
+                        }
+                        Ok((state, Decl::new(id, ident, None)))
                     }
+                    Some(Token::Eq) => {
+                        let (mut state, expr) = exprs::parse(state)?;
+                        match state.tokens.pop_front() {
+                            Some(Token::Semicolon) => {
+                                if let Some(_) = state.vars.insert(ident.clone(), id.clone()) {
+                                    let e = format!("Duplicate variable declaration: {ident}");
+                                    state.errors.push(e);
+                                }
+                                Ok((state, Decl::new(id, ident, Some(expr))))
+                            }
+                            Some(token) => Err(format!("Expected `;` found: {token}")),
+                            None => Err("Unexpected end of input: expected `;`".into()),
+                        }
+                    }
+                    Some(token) => Err(format!("Expected `;` or intializer found: {token}")),
+                    None => Err("Unexpected end of input: expected `;` or intializer".into()),
                 }
-                Some(token) => Err(format!("Expected `;` or intializer found: {token}")),
-                None => Err("Unexpected end of input: expected `;` or intializer".into()),
-            },
+            }
             Some(token) => Err(format!("Expected identifier found: {token}")),
             None => Err(String::from("Unexpected end of input: expected identifier")),
         },
