b21f41556dbcc905801e712432c652f46eb24309
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-02-16 14:55:23 -0500

    not happy with code, restarting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
-
-use nora::{build, get_args};
-
-fn init_loggging(level: Level) -> Result<(), String> {
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(level)
-        .pretty()
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)
-        .map_err(|_| format!("Failed to initialize tracing subscriber"))
-}
-
-fn main() -> Result<(), String> {
-    init_loggging(Level::DEBUG)?;
-    build(get_args())
+fn main() {
+    println!("Hello world")
 }
