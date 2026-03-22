7804abc22081fc703531a63faa3a7655610ae3d1
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-03-22 17:16:04 -0400

    Minor test name modifications for clarify

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,24 +1,26 @@
-use std::path::Path;
-
 use assert_cmd::{Command, cargo};
 use fake::{Fake, faker::lorem};
 use predicates::str::contains;
 use tempfile::NamedTempFile;
 
 #[test]
-fn returns_error_if_path_does_not_exist() {
+fn test_returns_error_if_path_does_not_exist() {
     let path = format!("{}.c", lorem::en::Word().fake::<String>());
+
     let mut cmd = Command::new(cargo::cargo_bin!());
     cmd.arg(&path);
+
     let error = format!("File {path} does not exist");
     cmd.assert().failure().stderr(contains(error));
 }
 
 #[test]
-fn returns_error_if_path_does_not_have_c_extension() {
+fn test_returns_error_if_path_does_not_have_c_extension() {
     let file: NamedTempFile = NamedTempFile::with_suffix(".mock").unwrap();
+
     let mut cmd = Command::new(cargo::cargo_bin!());
     cmd.arg(file.path());
+
     let error = format!("File must be a .c file: {}", file.path().display());
     cmd.assert().failure().stderr(contains(error));
 }
