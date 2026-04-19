c9ab1583de6f9095de598ee2771377afe991b626
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-04-18 23:06:50 -0400

    new cli test for lex flag

--- tests/inputs.rs
+++ tests/inputs.rs
@@ -1,27 +1,24 @@
-use std::env::temp_dir;
 use std::fs::{remove_file, write};
 
 use assert_cmd::{Command, cargo};
 use predicates::str::contains;
-use rand::distr::{Alphanumeric, SampleString};
-use rand::rng;
+
+use nora::random_temp_file_path;
 
 #[test]
 fn cli_returns_error_if_path_does_not_exist() {
-    let path = format!("{}.c", Alphanumeric.sample_string(&mut rng(), 24));
+    let path = random_temp_file_path(Some("mock"), 24);
 
     let mut cli = Command::new(cargo::cargo_bin!());
     cli.arg(&path);
 
-    let error = format!("File {path} does not exist");
+    let error = format!("File {} does not exist", path.display());
     cli.assert().failure().stderr(contains(error));
 }
 
 #[test]
 fn cli_returns_error_if_path_does_not_have_c_extension() {
-    let filestem = Alphanumeric.sample_string(&mut rng(), 24);
-    let path = temp_dir().join(format!("{}.mock", filestem));
-
+    let path = random_temp_file_path(Some("mock"), 24);
     write(&path, "Hello World!").unwrap();
 
     let mut cmd = Command::new(cargo::cargo_bin!());
@@ -29,6 +26,17 @@ fn cli_returns_error_if_path_does_not_have_c_extension() {
 
     let error = format!("File must be a .c file: {}", path.display());
     cmd.assert().failure().stderr(contains(error));
+
+    remove_file(&path).unwrap();
+}
+
+#[test]
+fn cli_lex_flag_succeeds() {
+    let path = random_temp_file_path(Some("c"), 24);
+    write(&path, "int main(){return 0;}").unwrap();
+
+    let mut cmd = Command::new(cargo::cargo_bin!());
+    cmd.arg(&path).arg("--lex").assert().success();
 
     remove_file(&path).unwrap();
 }
