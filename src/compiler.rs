d5c64e9ff7c1df94e3774b250895a77f1f128522
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-06-22 23:09:35 -0400

    added compiler driver and basic scafold for mocks

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,51 +1,77 @@
-use std::fs::{read_to_string, remove_file};
-use std::path::Path;
+use std::{path, process::Output};
 
-use super::{Gcc, temp_file_path};
+use super::driver::CompilerStage;
 
-mod lexer;
 mod tokens;
 
-/// Describes the different compilation phases in a compiler.
-#[derive(PartialEq, Debug)]
-pub enum Phase {
-    Lex,
-    Parse,
-    Codegen,
+pub trait Executor {
+    fn execute(&self, args: &[&str]) -> Result<Output, String>;
 }
 
-/// Describes why the compilation process exited.
-#[derive(PartialEq, Debug)]
-pub enum ExitReason {
-    Completed,
-    StoppedAfter(Phase),
+struct GccExecutor {
+    command: String,
 }
 
-/// Runs the compilation pipeline on the given `.c` input file.
-pub fn compile(path: &Path, stop_after: Option<Phase>) -> Result<ExitReason, String> {
-    let gcc = Gcc::default();
-    let inter = temp_file_path(Some("i"), 24);
-
-    if let Err(err) = gcc.preprocess(path, &inter) {
-        return Err(format!("Failed to preprocess input: {err}"));
-    };
-    let _ = match read_to_string(&inter) {
-        Ok(contents) => match lexer::Lexer::default().lex(contents) {
-            Ok(tokens) => tokens,
-            Err(err) => {
-                let _ = remove_file(&inter);
-                return Err(format!("Failed to open file: {err}"));
-            }
-        },
-        Err(err) => {
-            let _ = remove_file(&inter);
-            return Err(format!("Failed to open file: {err}"));
-        }
-    };
-
-    let _ = remove_file(inter);
-    if stop_after.is_some_and(|sa| sa == Phase::Lex) {
-        return Ok(ExitReason::StoppedAfter(Phase::Lex));
+impl GccExecutor {
+    pub fn new(command: impl AsRef<str>) -> Self {
+        Self { command: command.as_ref().into() }
     }
-    Ok(ExitReason::Completed)
+}
+
+impl Default for GccExecutor {
+    fn default() -> Self {
+        Self { command: String::from("gcc") }
+    }
+}
+
+impl Executor for GccExecutor {
+    fn execute(&self, args: &[&str]) -> Result<Output, String> {
+        todo!()
+    }
+}
+
+pub trait Toolchain {
+    fn preprocess(&self, input: impl AsRef<path::Path>) -> Result<path::PathBuf, String>;
+    fn link(&self, input: impl AsRef<path::Path>) -> Result<path::PathBuf, String>;
+}
+
+pub struct GccToolchain {
+    executor: GccExecutor
+}
+
+impl GccToolchain {
+    pub fn new(command: impl AsRef<str>) -> Self {
+        let executor = GccExecutor::new(command);
+        GccToolchain { executor }
+    }
+}
+
+impl Default for GccToolchain {
+    fn default() -> Self {
+        Self::new("gcc")
+    }
+}
+
+impl Toolchain for GccToolchain {
+    fn preprocess(&self, input: impl AsRef<path::Path>) -> Result<path::PathBuf, String> {
+        todo!()
+    }
+
+    fn link(&self, input: impl AsRef<path::Path>) -> Result<path::PathBuf, String> {
+        todo!()
+    }
+}
+
+pub struct Compiler<T: Toolchain> {
+    toolchain: T,
+}
+
+impl<T: Toolchain> Compiler<T> {
+    pub fn new(toolchain: T) -> Self {
+        Self { toolchain }
+    }
+
+    pub fn run(input: impl AsRef<path::Path>, stage: Option<CompilerStage>) -> Result<(), String> {
+        todo!()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,12 @@
-use std::ffi::OsStr;
-use std::path::PathBuf;
-
-use clap::{Args, Parser};
-
-use nora::{Phase, compile};
-
-#[derive(Args, Debug)]
-#[group(multiple = false)]
-pub struct Stage {
-    #[arg(long, help = "Tokenize the program")]
-    lex: bool,
-
-    #[arg(long, help = "Generate an AST for the program")]
-    parse: bool,
-
-    #[arg(long, help = "Generate assembly instructions")]
-    codegen: bool,
-}
-
-#[derive(Parser, Debug)]
-#[command(about, version)]
-struct Cli {
-    #[arg(help = "Path to C source file")]
-    path: PathBuf,
-
-    #[command(flatten)]
-    stage: Stage,
-}
+use clap::Parser;
+use nora::compiler::Toolchain;
 
 fn main() -> Result<(), String> {
-    let cli = Cli::parse();
+    let driver = nora::driver::CompilerDriver::parse();
 
-    if !cli.path.exists() {
-        return Err(format!("File {} does not exist", cli.path.display()));
+    if driver.path.extension().is_none_or(|e| e != "c") {
+        let file_path = driver.path.display();
+        return Err(format!("Input file must be a valid .c file: {file_path}"));
     }
-    if cli.path.extension().unwrap_or(OsStr::new("")) != "c" {
-        return Err(format!("File must be a .c file: {}", cli.path.display()));
-    }
-    
-    let stop_after = if cli.stage.lex {
-        Some(Phase::Lex)
-    } else if cli.stage.parse {
-        Some(Phase::Parse)
-    } else if cli.stage.codegen {
-        Some(Phase::Codegen)
-    } else {
-        None
-    };
-    compile(&cli.path, stop_after).map(|_| ())
+    todo!()
 }
